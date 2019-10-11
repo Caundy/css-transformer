3289276e44b4b138a2dfee66d86189565ab0c139
Konrad Bartlewicz <konradbartlewicz@MacBook-Pro-Konrad.local>
2019-10-12 00:17:21 +0200

    Updates

---
+++ precache-manifest.c49562ed6bd1c92f1f5a592978435b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf5c743488360db63367ffe1b9ae516",
+    "url": "/css-transformer/index.html"
+  },
+  {
+    "revision": "884132ae15a2a59e093d",
+    "url": "/css-transformer/static/css/main.03dfacde.chunk.css"
+  },
+  {
+    "revision": "c8bbe72f9b1f2b4e6b98",
+    "url": "/css-transformer/static/js/2.87b03917.chunk.js"
+  },
+  {
+    "revision": "884132ae15a2a59e093d",
+    "url": "/css-transformer/static/js/main.46e1452c.chunk.js"
+  },
+  {
+    "revision": "e067625f3b1f8224f551",
+    "url": "/css-transformer/static/js/runtime-main.169c41d8.js"
+  }
+]);
